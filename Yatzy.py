48525fdd08a9486dc402934b9c9ac94b812d8c24
Roosa <roosa.tjurin@edu.turkuamk.fi>
2025-04-08 13:43:10 +0300

    Päätiedoston luominen

---
+++ Yatzy.py
@@ -0,0 +1,12 @@
+
+# Tästä alkaa yatzy...
+
+# import random
+
+# def nopat..? - [Kuka tekee]       (noppa.py)
+
+# Heittofunktio - [kuka tekee]      (heitto.py)
+
+# Laskufunktio [kuka tekee?]        (lasku.py)
+
+# from noppa import nopat
